79e86a94bda4160d9ada476894762cda359a30b3
Bret Petersen <bretpetersen82@gmail.com>
2022-10-19 22:06:47 -0500

    Added light saber animation

---
+++ beach-bumming-VR/Assets/LightSaber.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSaber : MonoBehaviour
+{
+    public Animator LightSaberAnimator;
+
+    public void TurnOnLightSaber()
+    {
+        LightSaberAnimator.SetTrigger("Turn LightSaber On");
+        LightSaberAnimator.ResetTrigger("Turn LightSaber Off");
+    }
+
+    public void TurnOffLightSaber()
+    {
+        LightSaberAnimator.SetTrigger("Turn LightSaber Off");
+        LightSaberAnimator.ResetTrigger("Turn LightSaber On");
+    }
+}
